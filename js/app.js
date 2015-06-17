4630348ebb60504174908dcf0804ae6ae22a97f9
thescifer <thescifer@gmail.com>
2015-06-16 23:53:38 -0700

    Completed Jquery Street Fighter Project

--- js/app.js
+++ js/app.js
@@ -8,15 +8,51 @@ $(document).ready(function() {
 		$('.ryu-still').show();		
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// play hadouken sound
-		//show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);		
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 		// ryu goes back to his ready position
+	})
+	$(document).keydown(function(x) {
+		if (x.which == 88) {
+			$('.ryu-throwing').hide();
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
 	});
+	$(document).keyup(function(x) {
+		if (x.which == 88) {
+			$('.ryu-throwing').hide();			
+			$('.ryu-cool').hide();
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+		}
+	});
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+	}
 });
+
+// /* javascript */
+// $(document).on('keydown', function(e) {
+//     if (e.keyCode === 88) { // 88 is the letter D on the keyboard
+//         $('.ken').addClass('punch');
+//         setTimeout(function() { $ken.removeClass('punch'); }, 150);
+//     }
+// });
